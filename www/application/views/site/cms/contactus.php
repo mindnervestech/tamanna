ff61f2e9f120f4fca9ca9101eed4a3897041211b
unknown <saurebh@socktail.com>
2015-11-25 18:48:31 +0530

    new files added

---
+++ www/application/views/site/cms/contactus.php
@@ -0,0 +1,79 @@
+<?php
+$this->load->view('site/templates/header_new_small');
+?>
+			<!--main content-->
+			<div class="page_section_offset" style="padding: 0 0 25px;">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-12 col-md-12 col-sm-12 m_bottom_30 m_xs_bottom_10">
+							<div class="iframe_map_container m_bottom_38 m_xs_bottom_30">
+								<img src="images/contactus.jpg" width=100% height="480" alt="Contact Socktail" title="Contact Socktail">		</div>
+							<div class="row">
+								<main class="col-lg-6 col-md-6 col-sm-6 m_xs_bottom_30">
+									<h5 class="color_dark tt_uppercase second_font fw_light m_bottom_13">Have Queries?</h5>
+									<hr class="divider_bg m_bottom_25">
+									<ul class="second_font m_bottom_25" >
+										<li>Read the most frequently asked questions here</li>
+									</ul>
+									<h5 class="color_dark tt_uppercase second_font fw_light m_bottom_13">Still can not find an answer?</h5>
+									<hr class="divider_bg m_bottom_25">
+									<ul class="second_font m_bottom_25">
+										<li>Feel free to contact us with service questions, business proposals or media inquiries. We are open all days: 0900-2100 hrs</li>
+									</ul>
+									<ul class="second_font vr_list_type_2 m_bottom_33 m_xs_bottom_30">
+										<li><i class="fa fa-phone color_dark fs_large"></i>7304 22 44 88</li>
+										<li class="w_break" data-icon=""><i class="fa fa-envelope color_dark"></i><a href="mailto:#" class="sc_hover d_inline_b">contact@socktail.com</a></li>
+									</ul>
+									<h5 class="color_dark tt_uppercase second_font fw_light m_bottom_13">Corporate Office</h5>
+									<hr class="divider_bg m_bottom_25">
+									<p class="second_font m_bottom_15">#2, Marine Tower, St Patrick’s Town, Hadapsar, Pune - 411013</p>
+
+								</main>
+								<section class="col-lg-6 col-md-6 col-sm-6">
+									<h5 class="color_dark tt_uppercase second_font fw_light m_bottom_13">Contact Form</h5>
+									<hr class="divider_bg m_bottom_25">
+									<p class="second_font m_bottom_14">Send an email. All fields with an <span class="color_red">*</span> are required.</p>
+									<form id="contactform" class="b_default_layout">
+										<ul>
+											<li class="row">
+												<div class="col-lg-6 col-md-6 col-sm-6 m_bottom_15">
+													<label class="second_font required d_inline_b m_bottom_5 clickable" for="cf_name">First Name</label><br>
+													<input type="text" name="cf_name" id="cf_name" class="tr_all w_full fw_light">
+												</div>
+												<div class="col-lg-6 col-md-6 col-sm-6 m_bottom_15">
+													<label class="second_font required d_inline_b m_bottom_5 clickable" for="cf_email">Email Address</label><br>
+													<input type="email" name="cf_email" id="cf_email" class="tr_all w_full fw_light">
+												</div>
+											</li>
+											<li class="m_bottom_15">
+												<label class="second_font d_inline_b m_bottom_5 clickable" for="cf_telephone">Telephone</label><br>
+												<input type="text" name="cf_telephone" id="cf_telephone" class="tr_all w_full fw_light">
+											</li>
+											<li class="m_bottom_5">
+												<label class="second_font d_inline_b m_bottom_5 clickable" for="cf_message">Message</label><br>
+												<textarea id="cf_message" name="cf_message" rows="6" class="tr_all w_full fw_light"></textarea>
+											</li>
+											<li>
+												<button class="button_type_2 black state_2 tr_all second_font fs_medium tt_uppercase d_inline_b"><span class="m_left_10 m_right_10 d_inline_b">Submit</span></button>
+											</li>
+										</ul>
+									</form>
+								</section>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		<!--footer-->
+	<?php
+$this->load->view('site/templates/footer');
+?></div>
+
+		<!--libs include-->
+		 
+
+		<!--theme initializer-->
+		<script src="js/themeCore.js"></script>
+		<script src="js/theme.js"></script>
+	</body>
+</html>
---
+++ www/application/views/site/cms/whyus.php
@@ -0,0 +1,69 @@
+<?php
+$this->load->view('site/templates/header_new_small');
+?>
+			<!--main content-->
+			<div class="page_section_offset" style="padding: 0 0 25px;">
+				<div class="container">
+					<div class="row">
+						<section class="col-lg-12 col-md-12 col-sm-12">
+							<div class="clearfix m_bottom_15 m_bottom_25">
+								<div class="t_xs_align_c f_left m_right_20 m_xs_bottom_15 f_xs_none"><img src="images/about_img_1.jpg" alt=""></div>
+								<h4 class="fw_light second_font color_dark m_bottom_27 tt_uppercase">Custom Made Furniture</h4>
+								<p class="fw_light m_bottom_14 p_top_4">Most of our furniture products are customizable as per your requirements. Tell us the change you want, we will get it done for you. Even if you have any other design in your mind, provide us a picture or drawing, and we will make it for you.</p>
+							</div>
+							<div class="clearfix m_bottom_15 m_bottom_25">
+								<div class="t_xs_align_c f_right m_left_20 m_xs_bottom_15 f_xs_none"><img src="images/about_img_1.jpg" alt=""></div>
+								<h4 class="fw_light second_font color_dark m_bottom_27 tt_uppercase">Easy Payment Options</h4>
+								<p class="fw_light m_bottom_14 p_top_4">You can shop with any of the following payment options with us: Credit Cards, Debit Cards and Netbanking. You can even opt for partial paymnet for large orders, call our customer care number to avail the same.</p>
+								<p class="fw_light m_bottom_14 p_top_4">We provide COD facility of upto Rs 20,000 orders.</p>
+							</div>
+							<div class="clearfix m_bottom_15 m_bottom_25">
+								<div class="t_xs_align_c f_left m_right_20 m_xs_bottom_15 f_xs_none"><img src="images/about_img_1.jpg" alt=""></div>
+								<h4 class="fw_light second_font color_dark m_bottom_27 tt_uppercase">Hand Picked Products, Unmatched Quality</h4>
+								<p class="fw_light m_bottom_14 p_top_4">All our sellers and products are hand picked. We ensure that all products sold on our website are of high quality.</p>
+								<p class="fw_light m_bottom_14 p_top_4">Our wooden furniture comes with one year warranty against manufacturing defects, termite and borers.</p>
+
+							</div>
+							<div class="clearfix m_bottom_15 m_bottom_25">
+								<div class="t_xs_align_c f_right m_left_20 m_xs_bottom_15 f_xs_none"><img src="images/about_img_1.jpg" alt=""></div>
+								<h4 class="fw_light second_font color_dark m_bottom_27 tt_uppercase">Timely Delivery</h4>
+								<p class="fw_light m_bottom_14 p_top_4">We strive to get our customers their products in the quickest possible time. Shipping timelines vary from product to product and are mentioned against each product.
+								We will deliver your new furniture to your door step, no matter which floor it is. All items are shipped through our designated reputed courier service. Customers are provided with a tracking number as soon as the order is dispatched.</p>
+							</div>
+							<div class="clearfix m_bottom_15 m_bottom_25">
+								<div class="t_xs_align_c f_left m_right_20 m_xs_bottom_15 f_xs_none"><img src="images/about_img_1.jpg" alt=""></div>
+								<h4 class="fw_light second_font color_dark m_bottom_27 tt_uppercase">Easy Return and Refund</h4>
+								<p class="fw_light m_bottom_14 p_top_4">We try our best to provide you quality products with no friction delivery, still in case of any kind of manufacturing defect or transit damage which cannot be repaired on spot, we provide option of return and refund.</p>
+							</div>
+							<b class="d_block fs_large m_bottom_25">What is Socktail's return process?</b>
+							<p class="fw_light m_bottom_14">1. Please call our Customer Service Team on 7304 22 44 88 or send us an email on contact@socktail.com mentioning your order number and the details of the item you wish to return.</p>
+							<p class="fw_light m_bottom_14">2. Please pack the unused item with all accessories, tags, boxes, bags, stickers, papers and with the original Invoice.</p>
+							<p class="fw_light m_bottom_14">3. We will get the package picked from your location using our shipping partners</p>
+							<p class="fw_light m_bottom_14">4. Please email us the tracking number for the item being returned and send us your bank details (Account Name, Account Number, Account Type, Bank name, Branch Name and IFSC Code) for reimbursement of the payment in case payment mode was COD.</p>
+							<p class="fw_light m_bottom_14">5. Upon receipt, the items will be checked for quality by our quality control team. A refund will be approved or rejected basis these quality checks and our Customer Service team will inform you on the status of your refund within 48 hrs of receipt of items.</p>
+							
+							<b class="d_block fs_large m_bottom_25">How and when I will get refund?</b>
+							<p class="fw_light m_bottom_14">1. All refunds processed are credited through the original mode of payment.</p>
+							<p class="fw_light m_bottom_14">2. That means, money originally paid through bank transfer will be transferred back to your bank account and payments made by credit card will be reversed and reflect on your credit card statement. Refunds will reflect under the name of “Redolive Platforms Pvt Ltd” (our parent company). </p>
+							<p class="fw_light m_bottom_14">3. You will receive an email once the refund is initiated by us. Approved refund will be initiated within 48 hrs of receipt of items. </p>
+							<p class="fw_light m_bottom_14">4. From then it takes the payment gateway and the banks about 7-10 working days to credit the refund to your account. </p>
+						
+							<b class="d_block fs_large m_bottom_25">How can I cancel my order?</b>
+							<p class="fw_light m_bottom_14">Since most of our furniture is custom made for you, we accept cancellation up to 24 hours of placing the order. For orders cancelled within 24 hrs of placing the order, full amount paid will be refunded within 48 hrs to original mode pf payment. Even after 24 hours, you can give us a call on 7304 22 44 88 and find out the possibility of cancelling the order. Cancellation charges may apply for such scenarios which will be informed by our customer care executive. The refund for the balance amount will be reversed and reflect on your credit card / net banking statement within 7 to 10 working days. Refunds will reflect under the name of “Redolive Platforms Pvt Ltd” (our parent company).</p>
+						</section>
+					</div>
+				</div>
+			</div>
+			<!--footer-->
+				<?php
+					$this->load->view('site/templates/footer');
+				?>
+		</div>
+
+		<!--back to top-->
+		<button class="back_to_top animated button_type_6 grey state_2 d_block black_hover f_left vc_child tr_all"><i class="fa fa-angle-up d_inline_m"></i></button>
+		<!--theme initializer-->
+		<script src="js/themeCore.js"></script>
+		<script src="js/theme.js"></script>
+	</body>
+</html>
